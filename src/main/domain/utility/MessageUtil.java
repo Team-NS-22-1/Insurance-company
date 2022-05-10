3962f1847ce10f829e19f6e497a4a7b8f15e0712
kgh2120 <kgh2120@naver.com>
2022-05-10 17:24:15 +0900

    MessageUtil, ViewLogic 작성 내용 추가

--- src/main/domain/utility/MessageUtil.java
+++ src/main/domain/utility/MessageUtil.java
@@ -5,7 +5,7 @@ package main.domain.utility;
  * fileName : MessageUtil
  * author :  규현
  * date : 2022-05-10
- * description :
+ * description : menu와 목록을 넣으면 메세지를 뽑아주는 유틸리티 클래스
  * ===========================================================
  * DATE                 AUTHOR                NOTE
  * -----------------------------------------------------------
