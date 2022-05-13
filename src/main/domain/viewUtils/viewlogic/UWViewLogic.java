e9df693e8b744b93e7bdc05186c4a98b884611e3
yunyc <yunyc5233@gmail.com>
2022-05-14 05:12:01 +0900

    인수심사 1차 완성

--- src/main/domain/viewUtils/viewlogic/UWViewLogic.java
+++ src/main/domain/viewUtils/viewlogic/UWViewLogic.java
@@ -12,6 +12,8 @@ import main.domain.insurance.InsuranceListImpl;
 import main.domain.insurance.InsuranceType;
 import main.domain.viewUtils.ViewLogic;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -32,16 +34,9 @@ public class UWViewLogic implements ViewLogic {
 
     private Scanner sc;
     private Employee employee;
-    private String nextCommand;
-    private boolean exitCommand;
-    private boolean isExitCommand;
-    private int contractId;
 
-    @Override
-    public void showMenu() {
-        createMenu("언더라이팅팀메뉴", "인수심사한다", "돌아간다");
 
-        this.exitCommand = false;
+    public UWViewLogic() {
         this.sc = new Scanner(System.in);
 
         this.employee = new Employee();
@@ -50,8 +45,6 @@ public class UWViewLogic implements ViewLogic {
                 .setDepartment(Department.UW)
                 .setPhone("010-1111-2222")
                 .setPosition(Position.DEPTMANAGER);
-
-
 
         Customer customer = new Customer();
         customer.setId(1);
@@ -104,119 +97,148 @@ public class UWViewLogic implements ViewLogic {
     }
 
     @Override
+    public void showMenu() {
+        createMenu("언더라이팅팀메뉴", "인수심사한다", "돌아간다");
+    }
+
+    @Override
     public void work(String command) {
+        boolean isExit = false;
 
-        switch (command) {
-            // 인수심사한다
-            case "1":
-                readContractList();
-                break;
-            // 돌아간다
-            case "2":
-                break;
-            default:
-                break;
+        while (isExit != true) {
 
-        }
-    }
-    public void readContractList() {
-        createMenu("보험 선택","건강보험", "자동차보험", "화재보험");
+            try {
+                switch (command) {
+                    case "1" -> selectInsuranceType();
+                    case "2" -> isExit = true;
+                    default -> throw new RuntimeException();
+                }
 
-        this.nextCommand = sc.next();
-        InsuranceType insuranceType = null;
-
-        switch (nextCommand) {
-            case "1":
-                insuranceType = InsuranceType.HEALTH;
-                break;
-            case "2":
-                insuranceType = InsuranceType.CAR;
-                break;
-            case "3":
-                insuranceType = InsuranceType.FIRE;
-                break;
-        }
-
-        while (this.isExitCommand != true) {
-            createMenu("계약 ID | 고객 이름 | 인수심사상태");
-            printContractList(this.employee.readContract(insuranceType));
-
-            this.exitCommand = false;
-            readContractInfo();
-        }
-
-    }
-
-    public void readContractInfo() {
-
-        while (this.exitCommand != true) {
-            createMenu("인수심사할 계약 ID를 입력하세요.(홈화면으로 돌아간다는 0번)");
-            this.contractId = sc.nextInt();
-
-            if (this.contractId == 0) {
-                this.isExitCommand = true;
-                break;
+            } catch (RuntimeException e) {
+                System.out.println("잘못된 명령을 입력했습니다. 다시 입력해주세요.");
+                command = sc.next();
             }
+        }
 
-            updateUwState();
+    }
+
+    public void selectInsuranceType() {
+        boolean isExit = false;
+
+        while (isExit != true) {
+
+            try {
+                createMenu("보험 선택","건강보험", "자동차보험", "화재보험", "이전 화면으로 돌아간다");
+
+                InsuranceType insuranceType = null;
+
+                switch (sc.next()) {
+                    case "1"-> { insuranceType = InsuranceType.HEALTH; readContract(insuranceType); }
+                    case "2"-> { insuranceType = InsuranceType.CAR; readContract(insuranceType); }
+                    case "3"-> { insuranceType = InsuranceType.FIRE; readContract(insuranceType); }
+                    case "4" -> isExit = true;
+                    default -> throw new RuntimeException();
+                }
+            } catch (RuntimeException e) {
+                System.out.println("잘못된 명령을 입력했습니다. 다시 입력해주세요.");
+            }
         }
     }
 
+    public void readContract(InsuranceType insuranceType) {
+        boolean isExit = false;
 
-    public void updateUwState() {
+        while (isExit != true) {
 
-        while (this.exitCommand != true) {
-            createMenu("계약 정보(계약 ID: " + this.contractId + ")");
-            Contract contract = printContractInfo(this.contractId);
+            try {
+                createMenu("계약 ID | 고객 이름 | 인수심사상태");
+                printContractList(this.employee.readContract(insuranceType));
 
+                createMenu("인수심사할 계약 ID를 입력하세요.(이전 화면으로 돌아가기는 0번)");
+                String contractId = sc.next();
 
-            createMenu("명령 선택","승인", "거절", "재심사", "계약 목록 조회");
-            this.nextCommand = sc.next();
+                if (Integer.parseInt(contractId) == 0) break;
 
-            switch (nextCommand) {
+                createMenu("계약 정보(계약 ID: " + contractId + ")");
+                Contract contract = printContractInfo(Integer.parseInt(contractId));
 
-                case "1": case "2": case "3":
-                    createMenu("인수사유를 입력해주세요.");
-                    String reasonOfUw = sc.next();
-                    ConditionOfUw conditionOfUw = null;
+                selectUwState(contract);
 
-                    switch (nextCommand) {
-                        case "1":
-                            conditionOfUw = ConditionOfUw.APPROVAL;
-                            break;
-                        case "2":
-                            conditionOfUw = ConditionOfUw.REFUSE;
-                            break;
-                        case "3":
-                            conditionOfUw = ConditionOfUw.RE_AUDIT;
-                            break;
-                    }
-                    confirmUnderWriting(contract.getId(), reasonOfUw, conditionOfUw);
-                    break;
-
-                case "4":
-                    this.exitCommand = true;
-                    break;
-
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 명령을 입력했습니다. 다시 입력해주세요.");
+            } catch (NullPointerException e) {
+                System.out.println("계약 정보가 존재하지 않습니다.");
             }
         }
     }
 
 
-    public void confirmUnderWriting(int contractId, String reasonOfUw, ConditionOfUw conditionOfUw) {
-        createMenu("인수심사 결과를 반영하시겠습니까?", "예", "아니오");
+    public boolean selectUwState(Contract contract) {
+        boolean isExit = false;
 
-        switch (sc.next()) {
-            case "1":
-                employee.underwriting(contractId, reasonOfUw, conditionOfUw);
+        while (isExit != true) {
 
-                createMenu("인수심사 결과가 반영되었습니다.");
-                System.out.println(ContractListImpl.getContractList().get(1));
-                this.exitCommand = true;
-                break;
-            case "2":
-                break;
+            try {
+                createMenu("명령 선택","승인", "거절", "재심사", "계약 목록 조회");
+                String command = sc.next();
+
+                switch (command) {
+
+                    case "1": case "2": case "3":
+                        createMenu("인수사유를 입력해주세요.");
+                        String reasonOfUw = sc.next();
+                        ConditionOfUw conditionOfUw = null;
+
+                        switch (command) {
+                            case "1"-> conditionOfUw = ConditionOfUw.APPROVAL;
+                            case "2"-> conditionOfUw = ConditionOfUw.REFUSE;
+                            case "3"-> conditionOfUw = ConditionOfUw.RE_AUDIT;
+                            default -> new RuntimeException();
+                        }
+                        isExit = confirmUnderWriting(contract.getId(), reasonOfUw, conditionOfUw);
+                        break;
+
+                    case "4":
+                        return false;
+                    default:
+                        throw new RuntimeException();
+                }
+            } catch (RuntimeException e) {
+                System.out.println("잘못된 명령을 입력했습니다. 다시 입력해주세요.");
+            }
         }
+        return true;
+
+    }
+
+
+    public boolean confirmUnderWriting(int contractId, String reasonOfUw, ConditionOfUw conditionOfUw) {
+        boolean isExit = false;
+
+        while (isExit != true) {
+
+            try {
+                createMenu("인수심사 결과를 반영하시겠습니까?", "예", "아니오");
+
+                switch (sc.next()) {
+                    case "1":
+                        employee.underwriting(contractId, reasonOfUw, conditionOfUw);
+
+                        createMenu("인수심사 결과가 반영되었습니다.");
+                        System.out.println(ContractListImpl.getContractList().get(1));
+                        isExit = true;
+                        break;
+                    case "2":
+                        return false;
+                    default:
+                        throw new RuntimeException();
+                }
+            } catch (RuntimeException e) {
+                System.out.println("잘못된 명령을 입력했습니다. 다시 입력해주세요.");
+            }
+        }
+        return true;
+
     }
 
 
--- test/main/MainTest.java
+++ test/main/MainTest.java
@@ -1,8 +1,6 @@
 package main;
 
-import main.domain.contract.Contract;
-import main.domain.contract.ContractListImpl;
-import main.domain.contract.HealthInfo;
+import main.domain.contract.*;
 import main.domain.customer.Customer;
 import main.domain.customer.CustomerListImpl;
 import main.domain.insurance.Insurance;
@@ -16,12 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void main() {
+    void UWTest() {
         Customer customer = new Customer();
         customer.setId(1);
         customer.setAddress("주소");
         customer.setEmail("이메일");
         customer.setJob("직업");
+        customer.setName("홍길동");
 
         Insurance insurance = new Insurance();
         insurance.setId(1);
@@ -30,13 +29,29 @@ class MainTest {
         insurance.setInsuranceType(InsuranceType.HEALTH);
 
         HealthInfo healthInfo = new HealthInfo();
-        healthInfo.setDrinking(true);
+        CarInfo carInfo = new CarInfo();
+        BuildingInfo buildingInfo = new BuildingInfo();
 
         Contract testContract =  new Contract();
         testContract.setId(1);
         testContract.setCustomerId(1);
         testContract.setInsuranceId(1);
         testContract.setHealthInfo(healthInfo);
+        testContract.setConditionOfUw(ConditionOfUw.WAIT);
+
+        Contract testContract1 =  new Contract();
+        testContract1.setId(2);
+        testContract1.setCustomerId(1);
+        testContract1.setInsuranceId(1);
+        testContract1.setCarInfo(carInfo);
+        testContract1.setConditionOfUw(ConditionOfUw.WAIT);
+
+        Contract testContract2 =  new Contract();
+        testContract2.setId(3);
+        testContract2.setCustomerId(1);
+        testContract2.setInsuranceId(1);
+        testContract2.setBuildingInfo(buildingInfo);
+        testContract2.setConditionOfUw(ConditionOfUw.WAIT);
 
         CustomerListImpl customerList = new CustomerListImpl();
         customerList.create(customer);
@@ -46,6 +61,8 @@ class MainTest {
 
         ContractListImpl contractList = new ContractListImpl();
         contractList.create(testContract);
+        contractList.create(testContract1);
+        contractList.create(testContract2);
 
         Application app = new Application();
         app.run();
