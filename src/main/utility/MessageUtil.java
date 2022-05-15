ccbcbe2eb684666d261048e47c78a52c85c61795
kgh2120 <kgh2120@naver.com>
2022-05-15 15:15:13 +0900

    createMenuAndClose 메소드 생성

--- src/main/utility/MessageUtil.java
+++ src/main/utility/MessageUtil.java
@@ -23,6 +23,17 @@ public class MessageUtil {
         System.out.println(sb.toString());
     }
 
+    public static void createMenuAndClose(String menuName, String ... elements) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(menuName).append("\n");
+        for (int i = 0; i < elements.length; i++) {
+            sb.append(i + 1).append(".").append(elements[i]).append("\n");
+        }
+        sb.append("0.").append(" ").append("취소하기").append("\n")
+                        .append("exit.").append(" ").append("종료하기");
+        System.out.println(sb.toString());
+    }
+
     public static void createMenuEX(String menuName, UserType userType) {
         StringBuilder sb = new StringBuilder();
         sb.append(menuName).append("\n");
