8f59979ced79e859179ffc399d4c5489a49d5fa5
Seungho <hsh47607@naver.com>
2022-05-16 15:16:59 +0900

    Refactor MyBufferedReader whileTrue

--- src/main/utility/MyBufferedReader.java
+++ src/main/utility/MyBufferedReader.java
@@ -12,33 +12,34 @@ public class MyBufferedReader extends BufferedReader {
         super(in);
     }
 
-    public Object verifyRead(Object returnType) throws IOException,
-                                                    InputException.InputNullDataException,
-                                                    InputException.InputInvalidDataException,
-            MyCloseSequence {
-        String value = this.readLine();
-        checkBlankOrExit(value);
-
-        try {
-            if(returnType instanceof String)
-                return value;
-            else if(returnType instanceof Integer){
-                int intValue = Integer.parseInt(value);
-                return intValue;
+    public Object verifyRead(Object returnType) throws IOException {
+        while(true){
+            try {
+                String value = this.readLine();
+                checkBlankOrExit(value);
+                if(returnType instanceof String)
+                    return value;
+                else if(returnType instanceof Integer){
+                    int intValue = Integer.parseInt(value);
+                    return intValue;
+                }
+                else if(returnType instanceof Long){
+                    long longValue = Long.parseLong(value);
+                    return longValue;
+                }
+                else if(returnType instanceof Double){
+                    double doubleValue = Double.parseDouble(value);
+                    return doubleValue;
+                }
+                else
+                    throw new InputException.InputInvalidDataException();
             }
-            else if(returnType instanceof Long){
-                long longValue = Long.parseLong(value);
-                return longValue;
-            }
-            else if(returnType instanceof Double){
-                double doubleValue = Double.parseDouble(value);
-                return doubleValue;
-            }
-            else
+            catch (NumberFormatException e) {
                 throw new InputException.InputInvalidDataException();
-        }
-        catch (NumberFormatException e) {
-            throw new InputException.InputInvalidDataException();
+            }
+            catch (InputException e){
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -49,21 +50,25 @@ public class MyBufferedReader extends BufferedReader {
             throw new MyCloseSequence();
     }
 
-    public int verifyMenu(int categorySize) throws IOException,
-                                                    InputException.InvalidMenuException, MyCloseSequence {
-        String value = this.readLine();
-        checkBlankOrExit(value);
+    public int verifyMenu(int categorySize) throws IOException {
+        while(true){
+            try {
+                String value = this.readLine();
+                checkBlankOrExit(value);
 
-        int selectedMenu;
-        try {
-            selectedMenu = Integer.parseInt(value);
-            if(selectedMenu > categorySize || selectedMenu < 0)
+                int selectedMenu;
+                selectedMenu = Integer.parseInt(value);
+                if(selectedMenu > categorySize || selectedMenu < 0)
+                    throw new InputException.InvalidMenuException();
+                return selectedMenu;
+            }
+            catch (NumberFormatException e){
                 throw new InputException.InvalidMenuException();
+            }
+            catch (InputException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        catch (NumberFormatException e){
-            throw new InputException.InvalidMenuException();
-        }
-        return selectedMenu;
     }
 
 }
