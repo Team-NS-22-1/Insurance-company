dd68f35b796cd6aa425e1e09c7a510ac8dabd4b7
Seungho <hsh47607@naver.com>
2022-05-27 00:38:05 +0900

    insert dao package

--- src/main/java/domain/insurance/DevInfo.java
+++ src/main/java/domain/insurance/DevInfo.java
@@ -10,28 +10,31 @@ import java.time.LocalDate;
  */
 public class DevInfo {
 
+	private int id;
+	private int insuranceId;
 	private int employeeId;
 	private LocalDate devDate;
 	private SalesAuthState salesAuthState;
 	private LocalDate salesStartDate;
 
-	public DevInfo(){
+	public int getId() {
+		return id;
 	}
 
-	public String print() {
-		String value =
-				"{개발직원ID: " + employeeId +
-				", 개발일자: " + devDate.getYear() + "년 " +
-							devDate.getMonthValue() + "월 "+
-							devDate.getDayOfMonth() + "일" +
-				", 판매인가상태: " +salesAuthState.getName();
-		if(salesAuthState == SalesAuthState.PERMISSION) {
-			value += ", 판매시작일자: " + salesStartDate.getYear() + "년 " +
-									salesStartDate.getMonthValue() + "월 " +
-									salesStartDate.getDayOfMonth() + "일";
-		}
-		return value;
+	public DevInfo setId(int id) {
+		this.id = id;
+		return this;
 	}
+
+	public int getInsuranceId() {
+		return insuranceId;
+	}
+
+	public DevInfo setInsuranceId(int insuranceId) {
+		this.insuranceId = insuranceId;
+		return this;
+	}
+
 	public int getEmployeeId() {
 		return employeeId;
 	}
@@ -66,5 +69,20 @@ public class DevInfo {
 	public DevInfo setSalesStartDate(LocalDate salesStartDate) {
 		this.salesStartDate = salesStartDate;
 		return this;
+	}
+
+	public String print() {
+		String value =
+				"{개발직원ID: " + employeeId +
+				", 개발일자: " + devDate.getYear() + "년 " +
+							devDate.getMonthValue() + "월 "+
+							devDate.getDayOfMonth() + "일" +
+				", 판매인가상태: " +salesAuthState.getName();
+		if(salesAuthState == SalesAuthState.PERMISSION) {
+			value += ", 판매시작일자: " + salesStartDate.getYear() + "년 " +
+									salesStartDate.getMonthValue() + "월 " +
+									salesStartDate.getDayOfMonth() + "일";
+		}
+		return value;
 	}
 }
