a29ea170f65a6e6e7c19751d0d2496815b756323
kgh2120 <kgh2120@naver.com>
2022-05-31 00:15:04 +0900

    close() 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-//        InsuranceDao insuranceDao = new InsuranceDao();
-//        DevInfo devInfo = insuranceDao.readDevInfo(1);
-//        System.out.println("devInfo.getSalesAuthState() + " + devInfo.getDevDate() );
         Application app = new Application();
         app.run();
     }
