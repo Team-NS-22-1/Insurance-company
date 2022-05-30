ba7dac254ccf1a066a89dff4b07c2166753ff161
kgh2120 <kgh2120@naver.com>
2022-05-31 00:34:28 +0900

    temp

--- src/test/java/uwDao/ContractDaoTest.java
+++ src/test/java/uwDao/ContractDaoTest.java
@@ -1,5 +1,6 @@
 package uwDao;
 
+import dao.Dao;
 import domain.contract.CarContract;
 import domain.contract.Contract;
 import org.junit.jupiter.api.Test;
@@ -19,8 +20,18 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ContractDaoTest {
 
+    ContractDao dao = new ContractDao();
     @Test
     void updateTest() {
+        Contract con = new Contract();
+        con.setPremium(10000);
+        con.setCustomerId(22)
+                .setEmployeeId(2)
+                .setInsuranceId(3);
+
+
+            dao.create(con);
+
 
 
 
