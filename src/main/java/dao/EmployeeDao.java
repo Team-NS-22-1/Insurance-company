6a461de23cde720d92caca6d2050a1218ca6bfd5
Seungho <hsh47607@naver.com>
2022-05-30 22:03:35 +0900

    EmployeeDao

---
+++ src/main/java/dao/EmployeeDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+public class EmployeeDao extends Dao {
+    public EmployeeDao() {
+        super.connect();
+    }
+
+    public void create() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+
+}
--- src/main/resources/database/ddl.sql
+++ src/main/resources/database/ddl.sql
@@ -106,18 +106,18 @@ is_publish_stock boolean,
 condition_of_uw varchar(15)
 );
 
-create table health_info(
+create table health_contract(
 contract_id int not null primary key,
 height int,
 weight int,
-is_dangert_activity boolean,
+is_danger_activity boolean,
 is_drinking boolean,
 is_smoking boolean,
 is_taking_drug boolean,
 foreign key (contract_id) references contract(contract_id) on delete cascade
 );
 
-create table car_info(
+create table car_contract(
 contract_id int not null primary key,
 car_no varchar(15),
 car_type varchar(15),
@@ -128,7 +128,7 @@ value int,
 foreign key (contract_id) references contract(contract_id) on delete cascade
 );
 
-create table building_info(
+create table fire_contract(
 contract_id int not null primary key,
 building_area int,
 building_type varchar(20),
