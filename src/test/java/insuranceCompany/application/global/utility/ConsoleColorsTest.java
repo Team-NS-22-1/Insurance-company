b70e6266b3375a6c312b6d1de3e5f6b474d946e3
kyoohyun <kgh2120@naver.com>
2022-06-04 22:49:00 +0900

    consoleColor 클래스 추가

---
+++ src/test/java/insuranceCompany/application/global/utility/ConsoleColorsTest.java
@@ -0,0 +1,21 @@
+package insuranceCompany.application.global.utility;
+
+import insuranceCompany.application.global.exception.MyInvalidAccessException;
+import org.junit.jupiter.api.Test;
+
+import static insuranceCompany.application.global.constant.ExceptionConstants.INPUT_DATA_ON_LIST;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleColorsTest {
+
+    @Test
+    void colorException() {
+        String error = "\033[1;31mERROR!! : 리스트에 있는 아이디를 입력해주세요.";
+        try {
+            throw new MyInvalidAccessException(error);
+        } catch (MyInvalidAccessException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
