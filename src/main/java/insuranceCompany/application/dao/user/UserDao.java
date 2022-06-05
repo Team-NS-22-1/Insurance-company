8d67ec00addde648f437c14315d515a1cf82d13e
Mincheol <ofin0613@naver.com>
2022-06-05 19:05:51 +0900

    commit

--- src/main/java/insuranceCompany/application/dao/user/UserDao.java
+++
@@ -1,8 +0,0 @@
-package insuranceCompany.application.dao.user;
-
-import insuranceCompany.application.dao.CrudInterface;
-import insuranceCompany.application.login.User;
-
-public interface UserDao extends CrudInterface<User> {
-    int login(String id, String password);
-}
